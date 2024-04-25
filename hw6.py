11ae8935690655bda04d885c4d30f8637bb86a4e
202010460 김재희 <72098101+hhnkewi@users.noreply.github.com>
2024-04-25 17:03:53 +0900

    Create hw6.py

---
+++ hw6.py
@@ -0,0 +1,13 @@
+for i in range(1,10):
+    for j in range(2,6):
+        print(f'{j} x {i} = {j * i}', end = '\t')
+        if j % 4 == 1 :
+            print()
+print()
+
+for i in range(1,10):
+    for j in range(6,10):
+        print(f'{j} x {i} = {j * i}',  end = '\t')
+        if j % 4 == 1 :
+         print()
+       
